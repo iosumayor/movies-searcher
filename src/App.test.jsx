9f964c1e611a3c30c9fa3c4ebf57525a410857b5
iosumayor <iosumayor23@gmail.com>
2025-06-24 21:39:57 +0200

    test: sube los test para la busqueda

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,52 @@
-import { render, screen } from "@testing-library/react";
-import { describe, it, expect } from "vitest";
-import App from "./App";
+import { render, screen, waitFor } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import { getFilms } from "./core/service/getFilms";
+import { App } from "./App";
+import userEvent from "@testing-library/user-event";
+
+vi.mock("./core/service/getFilms");
+
+const mockFilms = [
+  { id: 1, title: "Interstellar", year: 2014 },
+  { id: 2, title: "Inception", year: 2010 },
+  { id: 3, title: "Interest in AI", year: 2023 },
+];
 
 describe("App Component", () => {
+  beforeEach(() => {
+    getFilms.getAllFilms.mockResolvedValue(mockFilms);
+  });
+
   it("displays the subtitle", () => {
     render(<App />);
     const subtitle = screen.getByText(/encuentra tu película favorita/i);
     expect(subtitle).toBeInTheDocument();
+  });
+
+  it("muestra el título de la película varias veces", async () => {
+    render(<App />);
+    await waitFor(() => {
+      const titles = screen.getAllByText("Interstellar");
+      expect(titles.length).toBeGreaterThanOrEqual(1);
+      titles.forEach((title) => expect(title).toBeInTheDocument());
+    });
+  });
+
+  it("filtra las películas que coinciden con el término de búsqueda y pueden aparecer varias veces", async () => {
+    render(<App />);
+
+    const input = screen.getByPlaceholderText("Buscar películas por título...");
+
+    await userEvent.type(input, "inter");
+
+    await waitFor(() => {
+      const interstellarMatches = screen.getAllByText("Interstellar");
+      expect(interstellarMatches.length).toBeGreaterThanOrEqual(1);
+
+      const interestMatches = screen.getAllByText("Interest in AI");
+      expect(interestMatches.length).toBeGreaterThanOrEqual(1);
+
+      expect(screen.queryByText("Inception")).not.toBeInTheDocument();
+    });
   });
 });
